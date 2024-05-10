398c66d132c3a4d91886570d7645e6f0c3d81ce8
Joban-Natt <jobanpreetnatt29@gmail.com>
2024-05-09 22:40:27 -0400

    Removing Expand

---
+++ app/components/redirects.tsx
@@ -0,0 +1,22 @@
+"use client";
+import React from "react";
+
+export function GitHub() {
+  return (
+    <div className="">
+      <button className="btn btn-primary" onClick={() => console.log("Click")}>
+        GitHub
+      </button>
+    </div>
+  );
+}
+
+export function youtube() {
+  return (
+    <div className="">
+      <button className="btn btn-primary" onClick={() => console.log("Click")}>
+        Youtube
+      </button>
+    </div>
+  );
+}
