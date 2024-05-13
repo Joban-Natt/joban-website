3a7eeb1b8646a7abd66c2c00900c4891a3bfb971
Joban-Natt <jobanpreetnatt29@gmail.com>
2024-05-12 23:44:28 -0400

    adding icon not working

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
+import Head from "next/head";
 import ProductCard from "./components/Projects/projectCard";
 import Intro from "./components/Intro/page";
 import Header from "./components/header/page";
@@ -9,6 +10,9 @@ import Footer from "./components/footer/page";
 export default function Home() {
   return (
     <main className="bg-jbgGrey">
+      <Head>
+        <link rel="shortcut icon" href="/favicon.ico" />
+      </Head>
       <Header />
       <Intro />
       <Experience />
